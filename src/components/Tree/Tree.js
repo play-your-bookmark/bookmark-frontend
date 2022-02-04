6f34cc50dd752ce6aa7de69fe6d500253d5be810
서현욱 <seo.hw@seohyeon-ug-ui-MacBookPro.local>
2022-02-05 01:03:33 +0900

    Feat: 추가 북마크편집기(addLink, removeLink) 추가 구현

--- src/components/Tree/Tree.js
+++ src/components/Tree/Tree.js
@@ -1,29 +1,38 @@
-import React, { useState } from "react";
-import { addFolder, buildTree, relocateFolder, removeFolder } from "../../utils/tree"
+import React from "react";
+import { addFolder, addLink, buildTree, relocateFolder, removeFolder, removeLink } from "../../utils/tree"
 import CATEGORY from "../../utils/category.json"
 
 export default function Tree () {
-  const [ newFolderCount, setNewFolderCount ] = useState(0);
-
   /* 
   1. build tree 
-  -> const folderTree = buildTree(0, []);
+  const folderTree = buildTree(0, []);
   */
  
   /* 
   2. relocate folder
-  -> relocateFolder(folderTree, "g", "f", "root");
+  relocateFolder(folderTree, "g", "f", "root");
   */
 
   /*
   3. add folder
-  -> addFolder(CATEGORY["mainCategory"][0]["subCategory"][5]["name"], newFolderCount, "파이썬 배우기", folderTree, "g");
+  addFolder(CATEGORY["mainCategory"][0]["subCategory"][5]["name"], newFolderCount, "파이썬 배우기", folderTree, "g");
   */ 
   
   /*
   4. remove folder
-  -> removeFolder("Python 0 g", "g", folderTree);
+  removeFolder("Python 0 g", "g", folderTree);
   */
+
+  /*
+  5. add Link
+  addLink(folderTree, "a", "root", "vanilla.com");
+  */
+
+  /*
+  6. remove Link
+  removeLink(folderTree, "a", "root", "vanilla.com");
+  */
+
  
   return (
     <div>
--- src/utils/tree.js
+++ src/utils/tree.js
@@ -10,7 +10,7 @@ export const buildTree = (level, folderTree) => {
     const temp = folderTree;
     for (let i = 1; i < FOLDERS.length; i++) {
       if (FOLDERS[i]['parent_folder'] === parentId) {
-        temp.push([FOLDERS[i].id, FOLDERS[i].title, []]);
+        temp.push([FOLDERS[i].id, FOLDERS[i].title, FOLDERS[i].bookmark]);
       }
     }
   
@@ -110,6 +110,69 @@ export const relocateFolder = (folderTree, targetId, currentParentId, newParentI
       const targetArray = currentSubtree.splice(i, 1);
       targetSubtree.push(targetArray[0]);
       break;
+    }
+  }
+}
+
+export const addLink = (folderTree, targetId, parentId, bookmark) => {
+  if (!folderTree[3]) {
+    return;
+  }
+
+  let targetArray = parentId === "root" ? folderTree : null;
+
+  for (let i = 3; i < folderTree.length; i++) {
+    if (targetArray) {
+      break;
+    }
+
+    if (folderTree[i][0] === parentId) {
+      targetArray = folderTree[i];
+      break;
+    }
+
+    addLink(folderTree[i], targetId, parentId);
+  }
+  for (let i = 3; i < targetArray.length; i++) {
+    if (targetArray[i][0] === targetId) {
+      targetArray[i][2].push(bookmark);
+      return;
+    }
+  }
+}
+
+export const removeLink = (folderTree, targetId, parentId, bookmark) => {
+  if (!folderTree[3]) {
+    return;
+  }
+
+  let targetArray = parentId === "root" ? folderTree : null;
+  let bookmarkArray = null;
+
+  for (let i = 3; i < folderTree.length; i++) {
+    if (targetArray) {
+      break;
+    }
+
+    if (folderTree[i][0] === parentId) {
+      targetArray = folderTree[i];
+      break;
+    }
+
+    removeLink(folderTree[i], targetId, parentId, bookmark);
+  }
+
+  for (let i = 3; i < targetArray.length; i++) {
+    if (targetArray[i][0] === targetId) {
+      bookmarkArray = targetArray[i];
+      break;
+    }
+  }
+
+  for (let i = 0; i < bookmarkArray.length; i++) {
+    if (bookmarkArray[i] === bookmark) {
+      bookmarkArray.splice(i, 1);
+      return;
     }
   }
 }
