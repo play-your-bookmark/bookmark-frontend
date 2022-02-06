da6c07276c3a0ce44b1728e8384154bb4a376acb
서현욱 <seo.hw@seohyeon-ug-ui-MacBookPro.local>
2022-02-06 16:19:45 +0900

    Remove: 삭제 mock code 삭제

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import { useRoutes } from "react-router-dom"
 import routes from "./routes";
 
 function App({ auth }) {
-  auth.logout();
   const routing = useRoutes(routes(auth));
 
   return routing;
