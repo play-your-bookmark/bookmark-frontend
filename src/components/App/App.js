64ad5716104e89ea06fd49d653b5a48087df1aad
hatae94 <gkxo5959@naver.com>
2022-02-08 21:00:48 +0900

    Feat: 추가 web-dev-server 프록시 설정

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import { useRoutes } from "react-router-dom";
+import model from "../../model/model";
 import routes from "./routes";
 
 function App({ auth }) {
   const routing = useRoutes(routes(auth));
-
+  model.get();
   return routing;
 }
 
