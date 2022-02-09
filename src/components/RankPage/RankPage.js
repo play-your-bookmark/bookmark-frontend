c42e38515535bd9703546999adc94fc5473d67fd
hatae94 <gkxo5959@naver.com>
2022-02-10 00:28:29 +0900

    Refactor: 수정 랭크페이지 LinkList 컴포넌트 삭제

--- src/components/RankPage/RankPage.js
+++ src/components/RankPage/RankPage.js
@@ -1,12 +1,9 @@
 import React from "react";
 
-import LinkList from "../Link/LinkList";
-
 export default function RankPage({ auth }) {
   return (
     <div>
       <h1>랭크페이지입니다</h1>
-      <LinkList />
     </div>
   );
 }
