e56112ed06aaabc4ba8e79b6dc5fdcadc2dc161c
hatae94 <gkxo5959@naver.com>
2022-02-09 22:13:54 +0900

    Refactor: 수정 검색기록 fetch 로직 및 req 유틸리티

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,9 @@
 import { useRoutes } from "react-router-dom";
-import model from "../../model/model";
 import routes from "./routes";
 
 function App({ auth }) {
   const routing = useRoutes(routes(auth));
-  model.get();
+
   return routing;
 }
 
--- src/components/RankPage/RankPage.js
+++ src/components/RankPage/RankPage.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import LinkList from "../Link/LinkList";
+
 export default function RankPage({ auth }) {
   return (
     <div>
       <h1>랭크페이지입니다</h1>
+      <LinkList />
     </div>
   );
 }
