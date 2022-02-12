d7f3c6fd76c0927ec7b50dc9229860cb1762efb3
sihyunlee <39shlee@gmail.com>
2022-02-13 04:36:31 +0900

    fix conflict

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -4,11 +4,8 @@ import logger from "redux-logger";
 import folderReducer from "../slices/folderSlices";
 import linkReducer from "../slices/linkSlices";
 import keywordReducer from "../slices/keywordSlices";
-<<<<<<< HEAD
 import categoryFolderReducer from "../slices/categoryFolderSlices";
-=======
 import userSlices from "../slices/userSlices";
->>>>>>> 0f8f998db7c6228fd3ea2cf455bdc4d0cfdceded
 
 const store = configureStore({
   reducer: {
