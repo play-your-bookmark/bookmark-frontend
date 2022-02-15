fc7fffc6bfde709edd341de1582eaa0750b799b8
서현욱 <hyunwookkotra@gmail.com>
2022-02-15 16:11:56 +0900

    Fix: 수정 folderSlice fetchCreatedFolder conflic 해결

--- src/redux/slices/folderSlices.js
+++ src/redux/slices/folderSlices.js
@@ -5,8 +5,14 @@ export const fetchCreatedFolder = createAsyncThunk(
   "get/folders",
   async (payload, { rejectWithValue, getState, dispatch }) => {
     try {
-      const { data } = await req("get", "/folder/main", {}, (res) => res, true);
+      const objectId = payload.userObjectId.id;
 
+      if (objectId) {
+        const { data } = await req("get", "/folder/main", { params: objectId }, (res) => res, true);
+        return data;
+      }
+
+      const { data } = await req("get", "/folder/main", {}, (res) => res, true);
       return data;
     } catch (error) {
       return rejectWithValue(error.response.data);
