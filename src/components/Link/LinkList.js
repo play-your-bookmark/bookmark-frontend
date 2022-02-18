3f9d08059a97231fb7c9662eaca3e78e028d1404
sihyunlee <39shlee@gmail.com>
2022-02-19 03:29:30 +0900

    add: 북마크 정보 가져오기

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 
+import axios from "axios";
+import Cookies from "js-cookie";
 import HistoryLink from "./HistoryLink";
 import Loader from "../Loader/Loader";
 import useInfinityScroll from "../hooks/useInfinityScroll";
@@ -34,12 +36,40 @@ export default function LinkList() {
   const [target, setTarget] = useState(null);
   const LinkLists = useSelector((state) => state.link.linkList);
   const isLoaded = useSelector((state) => state.link.isLoaded);
+  const [file, setFile] = useState(null);
+  const [bookmark, setBookmark] = useState([]);
 
   useInfinityScroll(target);
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const accessToken = Cookies.get("accessToken");
+    const formData = new FormData();
+    formData.append("bookmark", file);
+
+    const { data } = await axios.post("http://localhost:7001/file", formData, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    console.log(data);
+    setBookmark(data);
+  }
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
   return (
     <LinkListWrap>
-      {LinkLists && LinkLists.map((info, index) => <HistoryLink linkInfo={info} key={info.key} />)}
+      <form onSubmit={handleSubmit}>
+        <input name="html" type="file" accept="html" onChange={handleFileChange} />
+        <input type="submit" value="submit" />
+      </form>
+      {!!bookmark.length &&
+        bookmark.map((info, index) => <HistoryLink linkInfo={info} key={info.key} />)}
       <div className="Target-Element">
         <img ref={setTarget} className="logo-yellow" src={logoYellow} alt="logo_yellow" />
         {isLoaded && <Loader />}
