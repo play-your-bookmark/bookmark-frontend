38c76d3beabcf261c126eb63de8995fda3d4daff
서현욱 <hyunwookkotra@gmail.com>
2022-03-25 18:36:24 +0700

    [Fix] Timer 로직 수정

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -3,17 +3,22 @@ import styled from "styled-components";
 import ReactLoading from "react-loading";
 
 const LoaderWrap = styled.div`
-  width: 100%;
+  width: 110%;
   height: 80%;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   text-align: center;
   align-items: center;
+
+  .loading-time {
+    font-size: 15px;
+  }
 `;
 
-export default function Loader({ height = 64, width = 64 }) {
+export default function Loader({ height = 64, width = 64, loadingTime }) {
   return (
     <LoaderWrap>
+      <div className="loading-time">{loadingTime}초 뒤 업데이트</div>
       <ReactLoading type="spin" color="#A593E0" height={height} width={width} />
     </LoaderWrap>
   );
